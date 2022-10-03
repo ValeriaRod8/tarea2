23d4a362147cca08b633de2628f5c05bb86ca444
ValeriaRod8 <107070132+ValeriaRod8@users.noreply.github.com>
2022-10-02 23:11:07 -0600

    Tarea 2 Valeria Rodriguez

---
+++ src/main/java/tarea2/AplicacionesWP/AplicacionesWpApplication.java
@@ -0,0 +1,13 @@
+package tarea2.AplicacionesWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionesWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionesWpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tarea2/AplicacionesWP/AplicacionesWpApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea2.AplicacionesWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionesWpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
